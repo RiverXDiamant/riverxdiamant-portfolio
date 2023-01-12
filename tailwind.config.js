5729e853b850584297c97f38c951aa0e21cb5d0a
Alex <eddings.sharanna@gmail.com>
2023-01-12 15:41:39 -0500

    added screen breakpoints for tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    // set the break points for sm, md, lg and xl
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {},
   },
   plugins: [],
